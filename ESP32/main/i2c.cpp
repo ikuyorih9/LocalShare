fdb1bebefce36797e0a4ec4003259223089aff3e
Hugo Nakamura <hugohiroyuki9@gmail.com>
2024-12-01 22:34:56 -0300

    ESP32

--- ESP32/main/i2c.cpp
+++ ESP32/main/i2c.cpp
@@ -4,28 +4,36 @@
 #include <Wire.h>
 
 extern QueueHandle_t DataToSend;
+extern QueueHandle_t DataToReceive;
+
 
 void onRequest() {
 
   // Verifica o número de mensagens na fila
   if (uxQueueMessagesWaiting(DataToSend) == 0) {
-      Serial.println("A fila está vazia.");
+      // Serial.println("1: A fila está vazia.");
       Wire.write(-1);
       return;
   }
 
   uint8_t byte;
   if (xQueueReceive(DataToSend, &byte, portMAX_DELAY)) {
-      Serial.print("Enviando o byte");
-      Serial.println(byte);
       Wire.write(byte);
   }
 }
 
 void onReceive(int numBytes) {
   while (Wire.available()) {
-    char c = Wire.read(); // Lê dados do Master
-    Serial.print(c);
+    uint8_t reg = Wire.read(); // Lê dados do Master
+    if (reg == 0x01){
+      uint8_t byte = Wire.read(); // Lê dados do Master
+      xQueueSend(DataToReceive, &byte, portMAX_DELAY);
+    }
+    else if (reg == 0x02){
+      for(int i = 0; i < 4; i++){
+        uint8_t byte = Wire.read(); // Lê dados do Master
+        xQueueSend(DataToReceive, &byte, portMAX_DELAY);
+      }
+    }
   }
-  Serial.println();
 }
