cd8eb58e4dc7318c7ea32c5653a97dd4aff55b58
Hugo Nakamura <hugohiroyuki9@gmail.com>
2024-12-01 22:34:56 -0300

    ESP32

--- ESP32/main/main.ino
+++ ESP32/main/main.ino
@@ -2,11 +2,14 @@
 
 #include <Arduino.h>
 #include <Wire.h>
+#include <WiFi.h>
 #include "esp_task_wdt.h"
 #include "frtos.hpp"
 #include "i2c.hpp"
 
 QueueHandle_t DataToSend;
+QueueHandle_t DataToReceive;
+WiFiClient client;
 
 void setup() {
   esp_task_wdt_deinit();
@@ -16,8 +19,22 @@ void setup() {
   Wire.onReceive(onReceive);
   Wire.onRequest(onRequest);
 
-  DataToSend = xQueueCreate(32, sizeof(uint8_t));  // Criação da fila com capacidade de 10 elementos
+   // CONEXÃO WIFI
+  WiFi.begin(SSID, PASSWORD); // Inicia conexão wifi.
+  while (WiFi.status() != WL_CONNECTED) { // Enquanto não conecta
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // Task espera 1 segundo.
+  }
+
+  Serial.println("Wifi conectado!");
+
+  DataToSend = xQueueCreate(32, sizeof(uint8_t));
   if (DataToSend == NULL) {
+      Serial.println("Erro ao criar a fila");
+      while (1);
+  }
+
+  DataToReceive = xQueueCreate(32, sizeof(uint8_t));
+  if (DataToReceive == NULL) {
       Serial.println("Erro ao criar a fila");
       while (1);
   }
@@ -32,19 +49,25 @@ void setup() {
     0                  // Núcleo onde a tarefa será executada (0 para o núcleo 0)
   );
 
-  // Tarefa2 executando no Núcleo 1
-  // xTaskCreatePinnedToCore(
-  //   Tarefa2,           // Função da tarefa
-  //   "Tarefa2",         // Nome da tarefa
-  //   4096,              // Tamanho da pilha
-  //   NULL,              // Parâmetro
-  //   1,                 // Prioridade
-  //   NULL,              // Handle (opcional)
-  //   1                  // Núcleo onde a tarefa será executada (1 para o núcleo 1)
-  // );
+  //Tarefa2 executando no Núcleo 1
+  xTaskCreatePinnedToCore(
+    Tarefa2,           // Função da tarefa
+    "Tarefa2",         // Nome da tarefa
+    4096,              // Tamanho da pilha
+    NULL,              // Parâmetro
+    1,                 // Prioridade
+    NULL,              // Handle (opcional)
+    1                  // Núcleo onde a tarefa será executada (1 para o núcleo 1)
+  );
 
 }
 
 void loop() {
+  if(client.connect(IP, PORTA)){
+    Serial.println("Cliente conectado!");
+  }
+  while(client.connected());
+  client.stop();
+  Serial.println("Desconectado do servidor!");
   delay(100);
 }
--- SERVER/python/main.py
+++ SERVER/python/main.py
@@ -3,13 +3,16 @@ import os
 
 ip = "192.168.137.1"
 porta = 1234
+DELAY_BUFFER = 0.001
 
+command = 1
 username = input("Usuário: ")
 username_len = len(username)
-password = input("Senha:")
+password = input("Senha: ")
 password_len = len(password)
 # filename = input("Arquivo: ")
-filename = "./FILES/banana_1.png"
+filename = "banana_1.png"
+filepath = "../../FILES/banana_1.png"
 filename_len = len(filename)
 
 try:
@@ -22,7 +25,7 @@ try:
     print(f"Nova conexão de {addr}")
 
     try:
-        client.sendall(bytes([1]))
+        client.sendall(bytes([command]))
 
         print("Enviando tamanho do username...")
         client.sendall(bytes([username_len]))
@@ -34,16 +37,37 @@ try:
         print(f"Enviando a senha {password}")
         client.sendall(password.encode())
 
-        print("Enviando tamanho do nome arquivo...")
-        client.sendall(bytes([filename_len]))
-        print(f"Enviando o nome arquivo {filename}")
-        client.sendall(filename.encode())
-    
-        # tamanho = os.path.getsize(filename)
-        # print(f"Enviando tamanho do nome arquivo ({tamanho})({tamanho.to_bytes(4, byteorder='little')})...")
-        # client.sendall(tamanho.to_bytes(4, byteorder='little'))
-        # print("Enviando arquivo:")
-        # envia_arquivo(client, filename)
+        if command == 0:
+            print("Enviando tamanho do nome arquivo...")
+            client.sendall(bytes([filename_len]))
+            print(f"Enviando o nome arquivo {filename}")
+            client.sendall(filename.encode())
+        
+            tamanho = os.path.getsize(filepath)
+            print(f"Enviando tamanho do nome arquivo ({tamanho})({tamanho.to_bytes(4, byteorder='big')})...")
+            client.sendall(tamanho.to_bytes(4, byteorder='big'))
+            print("Enviando arquivo:")
+            envia_arquivo(client, filepath)
+        
+        elif command == 1:
+            print("Enviando tamanho do nome arquivo...")
+            client.sendall(bytes([filename_len]))
+            print(f"Enviando o nome arquivo {filename}")
+            client.sendall(filename.encode())
+            tam_bytes = []
+            for i in range (4):
+                tam_bytes.append(client.recv(1)[0])
+            tam = int.from_bytes(tam_bytes, byteorder='big')
+            
+            print(f"TAMANHO: {tam}")
+
+            with open("foto.png", "wb") as file:  # Abre ou cria o arquivo para escrita em modo binário
+                for i in range(tam):
+                    response = client.recv(1)  # Lê até 1024 bytes
+                    file.write(response)  # Escreve os bytes no arquivo
+                    print(f"{i} cliente: {response}")
+                    time.sleep(0.001)
+
         while True:
             pass
     except Exception as e:
